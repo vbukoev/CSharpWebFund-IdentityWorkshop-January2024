408862a09ba6ca1f6078599df3043f071e9fb3cc
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-01-31 00:48:47 +0200

    Create EntityValidationConstants.cs

---
+++ TaskBoardApp.Common/EntityValidationConstants.cs
@@ -0,0 +1,20 @@
+﻿namespace TaskBoardApp.Common
+{
+    public static class EntityValidationConstants
+    {
+        public static class Task
+        {
+            public const int TaskMaxTitle = 70;
+            public const int TaskMinTitle = 5;
+
+            public const int TaskMaxDescription = 1000;
+            public const int TaskMinDescription = 10;
+        }
+
+        public static class Board
+        {
+            public const int NameMinLength = 3;
+            public const int NameMaxLength = 30;
+        }
+    }
+}
