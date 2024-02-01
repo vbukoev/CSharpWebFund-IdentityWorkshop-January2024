c91981d6d79de95565de88ee4c7a14946134a878
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-02-01 20:08:16 +0200

    Create Task.cs

---
+++ TaskBoardApp.Data.Models/Task.cs
@@ -0,0 +1,36 @@
+﻿namespace TaskBoardApp.Data.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    using static TaskBoardApp.Common.EntityValidationConstants.Task;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using Microsoft.AspNetCore.Identity;
+    public class Task
+    {
+        public Task()
+        {
+            this.Id = Guid.NewGuid();
+        }
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required] 
+        [MaxLength(TaskMaxTitle)] 
+        public string Title { get; set; } = null!;
+
+        [Required]
+        [MaxLength(TaskMaxDescription)]
+        public string Description { get; set; } = null!;
+
+        public DateTime CreatedOn { get; set; }
+       
+        public int BoardId { get; set; }
+
+        public virtual Board Board { get; set; } = null!;
+
+        [Required]
+        [ForeignKey(nameof(Owner))]
+        public string OwnerId { get; set; } = null!;
+
+        public virtual IdentityUser Owner { get; set; } = null!;
+    }
+}
