bc9f35b4b055d91290cc592b176c465a2764b384
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-02-03 11:20:00 +0200

    Create IBoardService.cs

---
+++ TaskBoardApp.Services/Interfaces/IBoardService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskBoardApp.Web.ViewModels.Board;
+using TaskBoardApp.Web.ViewModels.Task;
+
+namespace TaskBoardApp.Services.Interfaces
+{
+    public interface IBoardService
+    {
+        Task<IEnumerable<BoardAllViewModel>> AllAsync();
+
+        Task<IEnumerable<BoardSelectViewModel>> AllForSelectAsync();
+
+        Task<bool> ExistsByIdAsync(int id);
+    }
+}
