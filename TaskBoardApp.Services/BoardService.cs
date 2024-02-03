8f88c40c10744d6eaf60ee21a8a92ae6b1e608d3
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-02-03 11:19:58 +0200

    Create BoardService.cs

---
+++ TaskBoardApp.Services/BoardService.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NuGet.Common;
+using TaskBoardApp.Data;
+using TaskBoardApp.Services.Interfaces;
+using TaskBoardApp.Web.ViewModels.Board;
+using TaskBoardApp.Web.ViewModels.Task;
+
+namespace TaskBoardApp.Services
+{
+    public class BoardService : IBoardService
+    {
+        private readonly TaskBoardDbContext dbContext;
+
+        public BoardService(TaskBoardDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        public async Task<IEnumerable<BoardAllViewModel>> AllAsync()
+        {
+            IEnumerable<BoardAllViewModel> boards = await this.dbContext
+                .Boards
+                .Select(b => new BoardAllViewModel()
+                {
+                Name = b.Name,
+                Tasks = b.Tasks.Select(t => new TaskViewModel
+                {
+                    Id = t.Id.ToString(),
+                    Title = t.Title,
+                    Description = t.Description,
+                    Owner = t.Owner.UserName
+                }).ToArray()
+            }).ToArrayAsync();
+
+            return boards;
+        }
+
+        public async Task<IEnumerable<BoardSelectViewModel>> AllForSelectAsync()
+        {
+            IEnumerable<BoardSelectViewModel> allBoards = await this.dbContext
+                .Boards
+                .Select(b => new BoardSelectViewModel()
+                {
+                    Id = b.Id,
+                    Name = b.Name
+                }).ToArrayAsync();
+
+            return allBoards;
+        }
+
+        public async Task<bool> ExistsByIdAsync(int id)
+        {
+            bool result = await this.dbContext.Boards.AnyAsync(b => b.Id == id);
+
+            return result;
+        } 
+    }
+}
