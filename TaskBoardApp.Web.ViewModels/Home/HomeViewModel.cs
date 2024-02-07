d07c6d267e2352c10e1431af0e69e484a1101880
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-02-07 09:53:53 +0200

    Create HomeViewModel.cs

---
+++ TaskBoardApp.Web.ViewModels/Home/HomeViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskBoardApp.Web.ViewModels.Home
+{
+    public class HomeViewModel
+    {
+        public int AllTasksCount { get; init; }
+
+        public List<HomeBoardModel> BoardsWithTasksCount { get; init; } = null!;
+        public int UserTasksCount { get; init; }
+    }
+}
