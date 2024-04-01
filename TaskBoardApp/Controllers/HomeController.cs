1227685e79d2932716b02c5790f102a2559a7601
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-04-01 17:12:20 +0300

    Create HomeController.cs

---
+++ TaskBoardApp/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using TaskBoardApp.Data;
+using TaskBoardApp.Extensions;
+using TaskBoardApp.Web.ViewModels.Home;
+
+namespace TaskBoardApp.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    public class HomeController : Controller
+    {
+        private readonly TaskBoardDbContext dbContext;
+        public HomeController(TaskBoardDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var taskBoards = dbContext
+                .Boards
+                .Select(b => b.Name)
+                .Distinct();
+
+            var tasksCount = new List<HomeBoardModel>();
+
+            foreach (var boardName in taskBoards)
+            {
+                var tasksInBoard = dbContext.Tasks.Count(t => t.Board.Name == boardName);
+
+                tasksCount.Add(new HomeBoardModel()
+                {
+                    BoardName = boardName,
+                    TasksCount = tasksInBoard
+                });
+            }
+
+            var userTasksCount = -1;
+
+            if (User.Identity.IsAuthenticated)
+            {
+                var currentUserId = User.GetId();
+                userTasksCount = dbContext.Tasks.Count(t => t.OwnerId == currentUserId);
+            }
+
+            var homeModel = new HomeViewModel()
+            {
+                AllTasksCount = dbContext.Tasks.Count(),
+                BoardsWithTasksCount = tasksCount,
+                UserTasksCount = userTasksCount
+            };
+
+            return View(homeModel);
+        }
+    }
+}
